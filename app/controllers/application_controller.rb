feed2f5089c99eb4d1e98c721f999844949b8843
jazzari <passionjazz@gmail.com>
2020-03-30 15:18:30 +0200

    Adding a translation to Spanish language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 	before_action :authorize 
-
+    before_action :set_i18n_locale_from_params 
 	
 
 	protected
@@ -9,4 +9,16 @@ class ApplicationController < ActionController::Base
 	  		redirect_to login_url, notice: "Please log in"
 	  	end
 	  end
+
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] = 
+            "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
 end
