6455a42c152e0eed496c7a113d4f0ae5e58d6cee
jazzari <passionjazz@gmail.com>
2020-03-31 10:10:02 +0200

    moving from Sqlite3 to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,19 +12,22 @@
 
 ActiveRecord::Schema.define(version: 2020_03_27_160718) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "line_items", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "cart_id"
+    t.bigint "product_id"
+    t.bigint "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity", default: 1
     t.decimal "price"
-    t.integer "order_id"
+    t.bigint "order_id"
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["order_id"], name: "index_line_items_on_order_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
@@ -55,4 +58,7 @@ ActiveRecord::Schema.define(version: 2020_03_27_160718) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "orders"
+  add_foreign_key "line_items", "products"
 end
